cd6996b7de629a25589f50c6fc9ee955226a08b5
Arthur <arrkhange1@pop-os.localdomain>
2023-07-19 21:51:02 +0500

    refact: move to angular v16

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  title = 'dsd';
+}
