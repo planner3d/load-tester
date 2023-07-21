a0706a7b4dc98ee2c97186559e1553289ca1de50
arrKhange1 <73338488+arrKhange1@users.noreply.github.com>
2023-07-21 22:32:22 +0500

    Настройка Angular проекта (#4)
    * refact: change css to scss
    
    * feat: set up routes
    
    ---------
    
    Co-authored-by: Arthur <arrkhange1@pop-os.localdomain>

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ActivatedRoute, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dsd';
+
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter} from "@angular/router";
+import {APP_ROUTES} from "./app/app-routing";
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideRouter(APP_ROUTES)
+    ]
+})
   .catch((err) => console.error(err));
