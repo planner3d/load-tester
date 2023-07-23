045b9764a1914f0f62b3290d57f8695afa2ae22f
Arthur <arrkhange1@pop-os.localdomain>
2023-07-23 19:39:43 +0500

    feat: add styles architecture

--- frontend/src/app/app-routing.ts
+++ frontend/src/app/app-routing.ts
@@ -1,15 +1,3 @@
 import {Routes} from "@angular/router";
 export const APP_ROUTES: Routes = [
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'auth'
-    },
-    {
-        path: 'auth',
-        loadComponent: () => import('../auth/auth.component')
-            .then(mod => mod.AuthComponent),
-        loadChildren: () => import('../auth/auth-routing')
-            .then(mod => mod.AUTH_ROUTES)
-    }
 ]
