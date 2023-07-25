d0b0c2d38e45f1f11ac11c55e459ff0d08433019
Arthur <therock65frostik@gmail.com>
2023-07-25 23:37:28 +0500

    feat: finish skeleton markup of expansion panel

--- frontend/src/shared/expansion-panel/expansion-panel.component.ts
+++ frontend/src/shared/expansion-panel/expansion-panel.component.ts
@@ -10,5 +10,5 @@ import { CommonModule } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExpansionPanelComponent {
-
+  protected bodyExpanded = false;
 }
