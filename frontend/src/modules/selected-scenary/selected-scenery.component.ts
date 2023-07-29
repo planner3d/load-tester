a15c4ad3d06bd6cc93adc70090f83b5747d0379a
Arthur <therock65frostik@gmail.com>
2023-07-29 14:21:57 +0500

    feat: add dynamic header to expansion panel

---
+++ frontend/src/modules/selected-scenary/selected-scenery.component.ts
@@ -0,0 +1,70 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ExpansionPanelComponent} from "../../shared/expansion-panel/expansion-panel.component";
+import {SceneryListComponent} from "../test-plan/features/scenery-list/scenery-list.component";
+import {HttpHeaderComponent} from "./features/http-header/http-header.component";
+
+export interface HttpSamplerRequest {
+  method: string;
+  url: string;
+}
+
+export interface HttpHeader {
+  domain: string;
+  method: string;
+  endpoint: string;
+}
+
+export interface HttpSampler {
+  httpHeader: HttpHeader;
+  requestBody?: HttpSamplerRequest;
+}
+
+export interface SelectedScenery {
+  sceneryHeader: string;
+  samplerList: HttpSampler[];
+}
+
+@Component({
+  selector: 'app-selected-scenery',
+  standalone: true,
+  imports: [CommonModule, SceneryListComponent, ExpansionPanelComponent, HttpHeaderComponent],
+  templateUrl: './selected-scenery.component.html',
+  styleUrls: ['./selected-scenery.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SelectedSceneryComponent {
+  protected selectedScenery: SelectedScenery = {
+    sceneryHeader: 'Тестовый сценарий 1',
+    samplerList: [
+      {
+        httpHeader: {
+          domain: 'www.google.com',
+          method: 'GET',
+          endpoint: '/'
+        },
+      },
+      {
+        httpHeader: {
+          domain: 'www.yandex.ru',
+          method: 'POST',
+          endpoint: '/api/music'
+        },
+      },
+      {
+        httpHeader: {
+          domain: 'www.amazon.com',
+          method: 'PUT',
+          endpoint: '/remove'
+        },
+      },
+      {
+        httpHeader: {
+          domain: 'www.kahoot.com',
+          method: 'GET',
+          endpoint: '/'
+        },
+      }
+    ],
+  }
+}
--- frontend/src/modules/test-plan/features/scenery-list/scenery-list.component.ts
+++ frontend/src/modules/test-plan/features/scenery-list/scenery-list.component.ts
@@ -2,13 +2,13 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-scenary-list',
+  selector: 'app-scenery-list',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './scenary-list.component.html',
-  styleUrls: ['./scenary-list.component.scss'],
+  templateUrl: './scenery-list.component.html',
+  styleUrls: ['./scenery-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScenaryListComponent {
-
+export class SceneryListComponent {
+  
 }
