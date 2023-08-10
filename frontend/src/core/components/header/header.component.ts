745d5e52232ee05b2e8e9ec6df7b24d9ccdf8981
Arthur <therock65frostik@gmail.com>
2023-08-10 23:53:46 +0500

    feat: connect to api

--- frontend/src/core/components/header/header.component.ts
+++ frontend/src/core/components/header/header.component.ts
@@ -1,14 +1,23 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
 
+  constructor(private http: HttpClient) {
+  }
+
+  protected testAPI(): void {
+    this.http.get(`${environment.apiUrl}/test-plan/element/children?parentGuid=guid-thread-group-1`)
+        .subscribe(console.log)
+  }
 }
