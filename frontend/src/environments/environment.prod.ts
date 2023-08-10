0f7c7b882611138d29064bb5e33f6d882cbd7151
arrKhange1 <73338488+arrKhange1@users.noreply.github.com>
2023-08-10 23:55:43 +0500

    setup connection to api  (#14)

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://backend.1531423-carruners.twc1.net/api'
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://backend.1531423-carruners.twc1.net/api'
+};
